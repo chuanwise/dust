abab207baf4a052d869a1aef7231ad32bde7cb59
Chuanwise <chuanwise@qq.com>
2023-04-11 23:37:45 +0800

    Add `BotFactory`

---
+++ dust-api/src/main/java/cn/chuanwise/dust/BotFactory.java
@@ -0,0 +1,9 @@
+package cn.chuanwise.dust;
+
+/**
+ * <h1>Bot 工厂</h1>
+ *
+ * @author Chuanwise
+ */
+public class BotFactory {
+}
---
+++ dust-api/src/main/java/cn/chuanwise/dust/factory/HttpBotFactory.java
@@ -0,0 +1,9 @@
+package cn.chuanwise.dust.factory;
+
+/**
+ * <h1>Http Bot 工厂</h1>
+ *
+ * @author Chuanwise
+ */
+public interface HttpBotFactory {
+}
